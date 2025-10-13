1507e02fa0430ef360b85787576cf061ceb95855
sofvanh <23138848+sofvanh@users.noreply.github.com>
2025-10-13 21:27:28 +0100

    Remove redundant

--- src/routes/+page.server.ts
+++
@@ -1,9 +0,0 @@
-export const actions = {
-  default: async ({ request }) => {
-    const formData = await request.formData();
-    const search = formData.get('search');
-    return {
-      search
-    }
-  }
-}
